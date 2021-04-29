94f635f57846bc97600cfe7693e45549b1428f9f
4lfman <31034983+4lfman@users.noreply.github.com>
2021-04-29 14:45:08 +0200

    Implemented factorial by the command fact(x)

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Numerics;
 
 namespace SigmaCalculator
 {
@@ -21,30 +22,27 @@ namespace SigmaCalculator
         private void CalculateButton_Click(object sender, EventArgs e)
         {
             double total = 0;
-            string formula = FormulaBox.Text;
-            var expr = new Expression(formula);
-            List<double> terms = new List<double>();
+            var expr = new Expression(FormulaBox.Text);
 
             for (int i = int.Parse(InitValueBox.Text); i <= int.Parse(UpperBoundBox.Text); i++)
             {
                 expr.Parameters["i"] = i;
 
-                //expr.EvaluateFunction += delegate (string name, FunctionArgs args)
-                //{
-                //    if (name == "Factorial")
-                //        args.Result = (int)args.Parameters[0].Evaluate() + (int)args.Parameters[1].Evaluate();
-                //};
-                //Func<int> f = expr.ToLambda<int>();
-                terms.Add(double.Parse(expr.Evaluate().ToString()));
+                expr.EvaluateFunction += delegate (string name, FunctionArgs args)
+                {
+                    if (name == "fact")
+                    {
+                        BigInteger argsFactorial = 1;
+
+                        for (int i = 2; i <= (int)args.Parameters[0].Evaluate(); i++)
+                        {
+                            argsFactorial *= i;
+                        }
+                        args.Result = argsFactorial;
+                    }
+                };
                 total += double.Parse(expr.Evaluate().ToString());
             }
-
-            //formula.IndexOf('!');
-            //while (formula.Contains("!"))
-            //{
-            //    formula = formula.Replace("!", "factorial()");
-            //}
-
             ResultLabel.Text = total.ToString();
         }
     }
